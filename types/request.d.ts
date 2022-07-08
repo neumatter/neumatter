5d89d0faa7e2a9b719f258c2145ba1f7d2858a40
Clyng57 <clyng57@gmail.com>
2022-07-08 13:12:48 -0500

    Update request.d.ts

--- types/request.d.ts
+++ types/request.d.ts
@@ -3,40 +3,33 @@ import { Socket } from 'net'
 import Neumatter from './index'
 
 declare class NeuRequest extends IncomingMessage {
+  body: object
+  params: object
+  app: Neumatter
+
   constructor (socket: Socket)
 
-  app: Neumatter
-  body: object
-  routePath: string
-  params: object
-  header: (name: string) => string|null
+  get (name: string|undefined): any
 
   get protocol (): string
   get isSecure (): boolean
   get host (): string
   get hostname (): string
-  get port (): number
+  get port (): number|undefined
   get path (): string
-
   get query (): object
-
   get href (): string
-
-  get ip (): Socket['remoteAddress']
-
-  get parseMethod (): string
-
-  set parseMethod (method)
-
+  get ip (): string|undefined
   get URL (): URL
   get searchParams (): URL['searchParams']
 
-  #setParams (hasParams: boolean, regexp: RegExp): Promise<this>
+  #setParams (hasParams: boolean, regexp: RegExp): Promise<NeuRequest>
 
   dispatch (hasParams: boolean, regexp: RegExp): Promise<void>
 
-  emitErr: (...rest: Array<any>) => any
-  isValidEmail: (email: string, message: string, subject: string) => Promise<object>
+  emitErr (...rest: any): any
+
+  isValidEmail(email: any, message?: string, subject?: string): Promise<{ valid: boolean; reason: string; }>
 }
 
 export default NeuRequest
